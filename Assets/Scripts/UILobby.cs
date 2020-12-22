169f7c31b66ea5f5580137212126926b443148e2
RIngo <wimberlyw@hotmail.com>
2020-12-22 00:07:20 -0500

    Beginnings of a lobby.

---
+++ Assets/Scripts/UILobby.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILobby : MonoBehaviour
+{
+    [SerializeField] InputField joinMatchInput;
+    [SerializeField] Button joinButton;
+    [SerializeField] Button hostButton;
+
+    public void Host()
+    {
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+        joinMatchInput.interactable = false;
+    }
+
+    public void Join()
+    {
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+        joinMatchInput.interactable = false;
+    }
+}
