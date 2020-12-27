352d94029580ac92bea9d64fc8102c18f0add95a
RIngo <wimberlyw@hotmail.com>
2020-12-27 15:03:59 -0500

    Now with title scene
    Work in "Play Scene" Now as its the offline version

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+        {
+            SceneManager.LoadScene("Play Scene");
+
+        }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PartnerPlayGame()
+    {
+        SceneManager.LoadScene("Offline");
+
+    }
+
+
+}
+
